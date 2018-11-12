4923d2678ac322ab658d19f6dd596f0e13756790
twinh <twinhuang@qq.com>
2018-11-12 18:24:39 +0800

    feat: raven改为自行合并的js

--- resources/views/raven/script.php
+++ resources/views/raven/script.php
@@ -1,9 +1,4 @@
-<script src="<?= $asset([
-  'comps/raven-js/dist/raven.js',
-  'comps/raven-js/plugins/native.js',
-  'comps/raven-js/plugins/jquery.js',
-  'comps/raven-js/plugins/require.js',
-]) ?>"></script>
+<script src="<?= $asset('comps/raven-js/raven.custom.min.js') ?>"></script>
 <script>
   Raven.config('<?= wei()->jsRaven->getDsn() ?>', {
       fetchContext: true
